9d47238462d4bf31924a33f2f3416a9ed363b3df
paul-talma <109061871+paul-talma@users.noreply.github.com>
2024-11-03 21:31:20 -0800

    implement clear_history()

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
 from main.run import interpret
 
 app = Flask(__name__)
+app.secret_key = "secretsecret"
 
-history = []
+
+@app.before_request
+def initialize_history():
+    if "history" not in session:
+        session["history"] = []
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", history=history)
+    return render_template("index.html", history=session["history"])
 
 
 @app.route("/evaluate", methods=["POST"])
@@ -16,9 +21,16 @@ def evaluate():
     expression = request.form["expression"]
     result = interpret(expression)
 
-    history.append({"expression": expression, "result": result})
+    session["history"].append({"expression": expression, "result": result})
+    session.modified = True
 
-    return render_template("index.html", history=history)
+    return redirect(url_for("index"))
+
+
+@app.route("/clear_history", methods=["POST"])
+def clear_history():
+    session["history"] = []
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
