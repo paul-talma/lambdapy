cf8e1be0c164483bd7ddce9beb2ecab7f926a72b
paul-talma <109061871+paul-talma@users.noreply.github.com>
2024-12-27 20:31:49 -0800

    correct error handling in app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from main.run import parse, interpret, transpile
+from main.parser import ParserError
 
 app = Flask(__name__)
 app.secret_key = "secretsecret"
@@ -19,23 +20,33 @@ def index():
 @app.route("/evaluate", methods=["POST"])
 def evaluate():
     expression = request.form["expression"]
-    input_tree = parse(expression)  # generate input tree
-    latex_input = transpile(input_tree)  # generate latex code for input representation
-    output_tree = interpret(input_tree)  # interpret expression
-    latex_output = transpile(
-        output_tree
-    )  # generate latex code for output representation
+    try:
+        input_tree = parse(expression)  # generate input tree
+        latex_input = transpile(
+            input_tree
+        )  # generate latex code for input representation
+        output_tree = interpret(input_tree)  # interpret expression
+        latex_output = transpile(
+            output_tree
+        )  # generate latex code for output representation
 
-    result = {
-        "expression": expression,
-        "latex_input": latex_input,
-        "latex_output": latex_output,
-    }
+        result = {
+            "expression": expression,
+            "latex_input": latex_input,
+            "latex_output": latex_output,
+        }
 
-    session["history"].append(result)
-    session.modified = True
+        session["history"].append(result)
+        session.modified = True
+        return redirect(url_for("index"))
 
-    return redirect(url_for("index"))
+    except ParserError as e:
+        app.logger.error(f"Error message: {e}.\n")
+        return render_template("index.html", history=session["history"], error=str(e))
+    except RecursionError as e:
+        return render_template(
+            "index.html", history=session["history"], error="Nice try ;)"
+        )
 
 
 @app.route("/clear_history", methods=["POST"])
